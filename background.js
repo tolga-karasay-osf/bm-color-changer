58973b4fecd60eb9057a9c95f89f9ffe64343cbf
tkarasay <tolga.karasay@osf.digital>
2023-05-10 23:05:46 +0300

    code optimization and adding comments

--- background.js
+++ background.js
@@ -1,37 +1,36 @@
 function runExtension() {
+  // if required page elements are not found, exit the function
   const bmHeader = document.querySelector('.slds-global-header');
   const bmBadge = document.querySelector('.slds-badge');
   const bmHeaderLogoImage = document.querySelector('.header__logo-image');
   const bmHeaderIcons = document.querySelectorAll('.slds-icon_small');
-  const kbdIcon = document.querySelector('.kbdIcon');
-
   if (!bmHeader || !bmBadge || !bmHeaderLogoImage || !bmHeaderIcons) {
     return;
   }
+  // define optional page element
+  const kbdIcon = document.querySelector('.kbdIcon');
 
+  // get sandbox ID from URL and define color key
   const sandboxId = window.location.hostname.split('.')[0];
   const colorKey = 'bmcc_' + sandboxId + '_color';
-  function setColor() {
-    if (bmHeader) {
-      chrome.storage.local.get([colorKey], (data) => {
-        bmHeader.classList.add(data[colorKey].bg);
-        bmBadge.classList.add(data[colorKey].bg);
-        bmHeaderLogoImage.classList.add('bmcc-color-filter-cloud');
-        bmHeaderIcons.forEach((icon) => {
-          icon.classList.add('bmcc-color-fill-' + data[colorKey].icon);
-        });
 
-        if (kbdIcon) {
-          kbdIcon.classList.add('bmcc-color-filter-' + data[colorKey].icon);
-        }
-      });
+  function setInitialColor(colorValue) {
+    bmHeader.classList.add(colorValue.bg);
+    bmBadge.classList.add(colorValue.bg);
+    bmHeaderLogoImage.classList.add('bmcc-color-filter-cloud');
+    bmHeaderIcons.forEach((icon) => {
+      icon.classList.add('bmcc-color-fill-' + colorValue.icon);
+    });
+    if (kbdIcon) {
+      kbdIcon.classList.add('bmcc-color-filter-' + colorValue.icon);
     }
   }
 
+  // get colorValue from local storage and run setInitialColor function
   chrome.storage.local.get([colorKey]).then((result) => {
     const colorValue = result[colorKey];
     if (colorValue && colorValue.bg !== 'default') {
-      setColor();
+      setInitialColor(colorValue);
     }
   });
 }
